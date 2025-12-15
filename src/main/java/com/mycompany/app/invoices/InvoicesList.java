41b6f1a46db9f806cc44b9d1c62ca2e6e3770b25
andrzejkorbiel <andkorbiel@gmail.com>
2025-12-15 13:15:28 +0100

    add date operations

--- src/main/java/com/mycompany/app/invoices/InvoicesList.java
+++ src/main/java/com/mycompany/app/invoices/InvoicesList.java
@@ -1,15 +1,19 @@
 package com.mycompany.app.invoices;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class InvoicesList {
   private Map<Integer, List<Invoice>> invoicesMap;
+  UserOperations userOperations;
+  DateOperations dateOperations;
 
   public InvoicesList(List<Invoice> invoices) {
-    UserOperations userOperations = new UserOperations();
-
+    this.userOperations = new UserOperations();
+    this.dateOperations = new DateOperations();
     this.invoicesMap = userOperations.mapInvoicesByUserId(invoices);
   }
 
@@ -21,10 +25,21 @@ public class InvoicesList {
     return invoicesMap.get(userId);
   }
 
-  public double calculateUserInvoicesGrossSum(int userId) {
-    UserOperations userOperations = new UserOperations();
+  public Map<Integer, List<Invoice>> getInvoicesByDate(Date date) {
+    return this.dateOperations.getInvoicesByDate(date);
 
-    return userOperations.calculateUserInvoicesGrossSum(userId);
+  }
+
+  public Map<Integer, List<Invoice>> getInvoicesFromDateRange(Date startDate, Date... endDate) {
+    return this.dateOperations.getInvoicesFromDateRange(startDate, endDate);
+  }
+
+  public double calculateUserInvoicesGrossSum(int userId) {
+    return this.userOperations.calculateUserInvoicesGrossSum(userId);
+  }
+
+  public double calculateUserTaxAmountSum(int userId) {
+    return this.userOperations.calculateUserTaxAmountSum(userId);
   }
 
   private class UserOperations {
@@ -36,6 +51,41 @@ public class InvoicesList {
       List<Invoice> userInvoices = InvoicesList.this.getInvociesByUsersId(userId);
 
       return userInvoices.stream().mapToDouble(Invoice::getGrossSum).sum();
+    }
+
+    public double calculateUserTaxAmountSum(int userId) {
+      List<Invoice> userInvoices = InvoicesList.this.getInvociesByUsersId(userId);
+      double sum = 0;
+
+      for (Invoice in : userInvoices) {
+        sum += in.getTaxAmount();
+      }
+
+      return sum;
+    }
+  }
+
+  private class DateOperations {
+    private Map<Integer, List<Invoice>> filterInvoices(Predicate<Invoice> predicate) {
+      return invoicesMap.entrySet().stream()
+          .map(e -> Map.entry(
+              e.getKey(),
+              e.getValue().stream()
+                  .filter(predicate)
+                  .collect(Collectors.toList())))
+          .filter(e -> !e.getValue().isEmpty())
+          .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public Map<Integer, List<Invoice>> getInvoicesByDate(Date date) {
+      return filterInvoices(inv -> inv.getDate().equals(date));
+    }
+
+    public Map<Integer, List<Invoice>> getInvoicesFromDateRange(Date startDate, Date... endDate) {
+      Date now = new Date();
+
+      return filterInvoices(inv -> inv.getDate().after(startDate)
+          && inv.getDate().before((endDate != null && endDate.length > 0) ? endDate[0] : now));
     }
   }
 }
--- src/test/java/com/mycompany/app/invoices/InvoicesListTest.java
+++ src/test/java/com/mycompany/app/invoices/InvoicesListTest.java
@@ -2,6 +2,7 @@ package com.mycompany.app.invoices;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +14,9 @@ import com.mycompany.app.products.ProductsList;
 import com.mycompany.app.taxes.TaxRate;
 
 public class InvoicesListTest {
-  java.util.Date date = new java.util.Date();
+  Date date = new java.util.Date();
+  Date tenDaysBefore = new Date(date.getTime() - (10 * 24 * 60 * 60 * 1000));
+
   private ProductWithQt productA = new ProductWithQt(1, "Product A", 10.0, 2);
   private ProductWithQt productB = new ProductWithQt(2, "Product B", 20.0, 1);
   private ProductWithQt productC = new ProductWithQt(3, "Product C", 120.0, 2);
@@ -33,7 +36,8 @@ public class InvoicesListTest {
 
     invoiceA = new Invoice(1, "Invoice A", date, TaxRate.TEN, productsList, 10);
     invoiceB = new Invoice(2, "Invoice B", date, TaxRate.EIGHTTEEN, productsList, 21);
-    invoiceC = new Invoice(3, "Invoice C", date, TaxRate.EIGHT, new ProductsList(List.of(productC)),
+    invoiceC = new Invoice(3, "Invoice C",
+        tenDaysBefore, TaxRate.EIGHT, new ProductsList(List.of(productC)),
         10);
 
     invoicesList = new InvoicesList(List.of(invoiceA, invoiceB, invoiceC));
@@ -54,15 +58,49 @@ public class InvoicesListTest {
   }
 
   @Test
+  public void testGetInvoicesByDate() {
+    assertEquals(Map.of(10, List.of(invoiceC)), invoicesList.getInvoicesByDate(tenDaysBefore));
+    assertEquals(Map.of(10, List.of(invoiceA), 21, List.of(invoiceB)), invoicesList.getInvoicesByDate(date));
+  }
+
+  @Test
+  public void testGetInvoicesByDateRange() {
+    Date twoWeeksBefore = new Date(date.getTime() - (14 * 24 * 60 * 60 * 1000));
+    Date fiveDaysBefore = new Date(date.getTime() - (5 * 24 * 60 * 60 * 1000));
+    Date oneDayBefore = new Date(date.getTime() - (1 * 24 * 60 * 60 * 1000));
+
+    assertEquals(Map.of(10, List.of(invoiceA, invoiceC), 21, List.of(invoiceB)),
+        invoicesList.getInvoicesFromDateRange(twoWeeksBefore));
+
+    assertEquals(Map.of(10, List.of(invoiceA), 21, List.of(invoiceB)),
+        invoicesList.getInvoicesFromDateRange(oneDayBefore));
+
+    assertEquals(Map.of(10, List.of(invoiceC)),
+        invoicesList.getInvoicesFromDateRange(twoWeeksBefore, fiveDaysBefore));
+  }
+
+  @Test
   public void testCalculateUserInvoicesGrossSum() {
     // invoiceA: 40 * 10% = 44
     // invoiceC: 240 * 8% = 259,2
     double expectedResultA = 303.2;
 
-    // invoiceB: 40* 18% 
+    // invoiceB: 40* 18%
     double expectedResultB = 47.2;
 
     assertEquals(expectedResultA, invoicesList.calculateUserInvoicesGrossSum(10));
     assertEquals(expectedResultB, invoicesList.calculateUserInvoicesGrossSum(21));
+  }
+
+  @Test
+  public void testCalculateUserTaxAmountSum() {
+    // 4 + 19,2
+    double expectedResultA = 23.2;
+
+    // invoiceB: 40 * 18%
+    double expectedResultB = 7.2;
+
+    assertEquals(expectedResultA, invoicesList.calculateUserTaxAmountSum(10));
+    assertEquals(expectedResultB, invoicesList.calculateUserTaxAmountSum(21));
   }
 }
